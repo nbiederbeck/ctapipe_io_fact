e049a5818731dbc034af54dcf3b9dd3d738bc27a
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-09-05 15:32:22 +0200

    Remove unused code

--- ctapipe_fact/zfits.py
+++ ctapipe_fact/zfits.py
@@ -1,7 +1,5 @@
-from zfits import FactFitsCalib, FactFits
+from zfits import FactFitsCalib
 from ctapipe.io.containers import DataContainer
-from ctapipe.io.eventfilereader import EventFileReader
-from traitlets import Unicode
 from astropy.time import Time
 
 from .instrument import FACT
@@ -13,8 +11,13 @@ def fact_event_generator(inputfile, drsfile):
     header = fact_fits_calib.data_file.header()
 
     for event in fact_fits_calib:
+        trigger_type = event['TriggerType'][0]
+
+        if trigger_type != 4:
+            continue
 
         event_id = event['EventNum'][0]
+
         night = header['NIGHT']
         run_id = header['RUNID']
 
@@ -35,31 +38,12 @@ def fact_event_generator(inputfile, drsfile):
             c.run_id = '{}_{:03d}'.format(night, run_id)
             c.tels_with_data = [0]
 
-        event['CalibData'][8::9, 240:300] = 0
+        event['CalibData'][:, 150:300] = 0
+        event['CalibData'][:, :10] = 0
 
         data.r0.tel[0].adc_samples = event['Data'].reshape(1, 1440, -1)
         data.r0.tel[0].num_samples = event['Data'].shape[1]
         data.r1.tel[0].pe_samples = event['CalibData'].reshape(1, 1440, -1) / 230
         data.dl0.tel[0].pe_samples = event['CalibData'].reshape(1, 1440, -1) / 230
 
-
         yield data
-
-
-class FACTEventFileReader(EventFileReader):
-    drs_file_path = Unicode(
-        allow_none=False,
-        help='Path to the DRS calibration file'
-    )
-    origin = 'FACT'
-
-    def __init__(self, config, tool, **kwargs):
-        super().__init__(config=config, tool=tool, **kwargs)
-
-    @staticmethod
-    def check_file_compatibility(file_path):
-        try:
-            f = FactFits(file_path)
-            return f.header().get('TELESCOP') == 'FACT'
-        except Exception as e:
-            return False
