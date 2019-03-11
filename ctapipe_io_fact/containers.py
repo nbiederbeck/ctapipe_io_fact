413fc312ec81fed7c6ca5b9317364fb7f8c88c77
Noah Biederbeck <noah.biederbeck@udo.edu>
2019-03-11 16:12:10 +0100

    use double quotes

--- ctapipe_io_fact/containers.py
+++ ctapipe_io_fact/containers.py
@@ -5,7 +5,7 @@ Container structures for data that should be read or written to disk
 from ctapipe.core import Container, Field, Map
 from ctapipe.io.containers import DataContainer
 
-__all__ = ['FACTContainer', 'FACTCameraContainer']
+__all__ = ["FACTContainer", "FACTCameraContainer"]
 
 
 class FACTServiceContainer(Container):
