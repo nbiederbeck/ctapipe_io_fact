6c5f15e8c6b99409efc29f660d3a182a692034ef
Noah Biederbeck <noah.biederbeck@udo.edu>
2019-05-22 16:42:14 +0200

    Add event_id, tels_with_data and obs_id

--- ctapipe_io_fact/__init__.py
+++ ctapipe_io_fact/__init__.py
@@ -1,6 +1,4 @@
-"""
-EventSource for FACT fits-files.
-"""
+"""EventSource for FACT fits-files."""
 
 from astropy.io import fits
 from astropy import units as u
@@ -16,9 +14,8 @@ __all__ = ["FACTDL1EventSource"]
 
 
 class FACTDL1EventSource(EventSource):
-    """
-    EventSource for FACT DL1 data.
-    """
+
+    """EventSource for FACT DL1 data."""
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -26,57 +23,45 @@ class FACTDL1EventSource(EventSource):
         self.hdulist = fits.open(self.input_url)
         self.events = self.hdulist["Events"].data
         self.header = self.hdulist["Events"].header
-        self.metadata["is_simulation"] = "timestamp" not in list(
-            self.header.values()
-        )
+        self.metadata["is_simulation"] = "timestamp" not in list(self.header.values())
 
     def _generator(self):
         for i in range(self.header["NAXIS2"]):
             event_data = self.events[i]
             event = DataContainer(count=i)
-            event.r0.obs_id = (
-                self.header["NIGHT"] * 1000 + self.header["RUNID"]
-            )
-            event.r0.event_id = event_data["event_num"]
+            for cont in ("r0", "r1", "dl0"):
+                event[cont].obs_id = self.header["NIGHT"] * 1000 + self.header["RUNID"]
+                event[cont].event_id = event_data["event_num"]
+                event[cont].tels_with_data = [1]
 
             if self.metadata["is_simulation"]:
                 event.mc.energy = u.Quantity(
                     event_data["corsika_event_header_total_energy"], u.GeV
                 )
-                event.mc.core_x = u.Quantity(
-                    event_data["corsika_event_header_x"], u.cm
-                )
-                event.mc.core_y = u.Quantity(
-                    event_data["corsika_event_header_y"], u.cm
-                )
+                event.mc.core_x = u.Quantity(event_data["corsika_event_header_x"], u.cm)
+                event.mc.core_y = u.Quantity(event_data["corsika_event_header_y"], u.cm)
                 event.mc.h_first_int = u.Quantity(
                     event_data["corsika_event_header_first_interaction_height"]
                 )
                 # event.mc.xmax = u.Quantity(, u.g / u.cm ** 2)
                 event.mc.shower_primary_id = 0
-                event.mc.alt = u.Quantity(
-                    90 - event_data["source_position_zd"], u.deg
-                )
-                event.mc.az = u.Quantity(
-                    event_data["source_position_az"], u.deg
-                )
+                event.mc.alt = u.Quantity(90 - event_data["source_position_zd"], u.deg)
+                event.mc.az = u.Quantity(event_data["source_position_az"], u.deg)
                 event.mc.tel[1] = DL1CameraContainer(
                     image=event_data["photoncharge"],
-                    peakpos=event_data["arrival_time"],
+                    pulse_time=event_data["arrival_time"],
                 )
             else:
                 event.dl0.tel[1].trigger_time = event_data["timestamp"]
 
                 event.dl1.tel[1] = DL1CameraContainer(
                     image=event_data["photoncharge"],
-                    peakpos=event_data["arrival_time"],
+                    pulse_time=event_data["arrival_time"],
                 )
 
             event.pointing = TelescopePointingContainer(
                 azimuth=u.Quantity(event_data["pointing_position_az"], u.deg),
-                altitude=u.Quantity(
-                    90 - event_data["pointing_position_zd"], u.deg
-                ),
+                altitude=u.Quantity(90 - event_data["pointing_position_zd"], u.deg),
             )
 
             yield event
